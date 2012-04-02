45247413a0fa8b461be0ab06e926a0a5fdb418a8
Влад <vvladxx@gmail.com>
2012-04-02 19:45:52 +0400

    Added tests

---
+++ tests/calloc.c
@@ -0,0 +1,20 @@
+#include "jemalloc.h"
+
+const size_t min_size = 1024 * 1;
+const size_t max_size = 1024 * 1024;
+const size_t num = 100;
+
+int main (void)
+{
+	size_t i;
+	char * ptrs[num];
+	for (i = 0; i < num; i++)
+	{
+		size_t size = (rand() % (max_size - min_size)) + min_size;
+		ptrs[i] = je_calloc(size, 1);
+		memset(ptrs[i], 1, size);
+	}
+	for (i = 0; i < num; i++)
+		je_free(ptrs[i]);
+	return 0;
+}
---
+++ tests/jemalloc.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
+
+void * je_malloc(size_t);
+void * je_calloc(size_t, size_t);
+void * je_realloc(void *, size_t);
+void je_free(void *);
